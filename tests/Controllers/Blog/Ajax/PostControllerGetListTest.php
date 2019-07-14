f09988f6e05faedb7ab904b7ef372f5cc9d49956
landon <sbshz85@gmail.com>
2019-07-14 22:51:33 +0900

    Add test

---
+++ tests/Controllers/Blog/Ajax/PostControllerGetListTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: landon
+ * Date: 2019-07-14
+ * Time: 22:45
+ */
+
+namespace Tests\Controllers\Blog\Ajax;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\MockHelper;
+use Tests\TestCase;
+
+class PostControllerGetListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        config(['app.url' => 'http://test-7772.blog.io']);
+    }
+
+    /**
+     * 게시물 리스트를 가져올 수 있다.
+     */
+    public function test()
+    {
+        // Given
+        $post = MockHelper::mockPost();
+
+        // When
+        $response = $this->get('/ajax/posts');
+
+        // Then
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'posts',
+            ]);
+    }
+
+}
